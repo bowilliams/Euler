dd08d704593ecbcbec37b5376bb23f04a1b314b8
Bo Williams <bowilliams@gmail.com>
2012-01-12 14:40:11 -0500

    remove WIP code from problem2

--- problem2.py
+++ problem2.py
@@ -1,6 +1,3 @@
-import time
-start = time.clock()
-print str(start)
 x = 1
 y = 2
 sum = 2
@@ -12,24 +9,9 @@ while ((x+y) < 4000000):
     y = i
     if (i % 2 == 0):
         sum += i
-end = time.clock()
-print str(end-start) + 'ms'
 print sum
 
 # optimization - after 2, every third term is an even term. can we just skip to those?
-start = time.clock()
-sum = 0
-a=1
-b=1
-c=a+b
-while (c<4000000):
-    sum += c
-    a = b+c
-    b = a+c
-    c = a+b
-end = time.clock()
-print str(end-start) + 'ms'
-print sum
 
 
 
